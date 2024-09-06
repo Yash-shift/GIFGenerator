b9a58fd07aa09fcd558ecc4da2afb02854aa56c6
Yash-shift <imyashdixit001@gmail.com>
2024-09-06 12:23:05 +0530

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Random from './Components/Random'
+import Tag from './Components/Tag'
+
+function App() {
+  return (
+
+
+    <div className=" relative bg-gradient-to-r from-sky-800 to-indigo-500 w-full  flex flex-col h-screen overflow-x-hidden">
+      <h1 className='absolute bg-slate-300 rounded-xl w-11/12 text-center mt-8  px-10 ml-16 py-3 text-3xl mx-auto font-bold'>
+        Hello, Gif Generator! 
+      </h1>
+      {/* <div>
+        <Random/>
+        <Tag/>
+      </div> */}
+      
+    </div>
+  );
+}
+
+export default App;
